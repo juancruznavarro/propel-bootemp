790765a460a530c789aa5f196aa544afbccb8985
Juan Cruz <jcruz@jcruz.(none)>
2014-08-09 21:04:06 -0300

    FIX-2

--- config/config.php
+++ config/config.php
@@ -1,14 +1,23 @@
 <?php
+echo '<pre>';
+print_r($_SERVER);
+
+echo $_SERVER['DOCUMENT_ROOT'];
+die;
+
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set("display_errors", 1);
 
+
+
+
 define('SITE_TITLE','Administrador :: ');
-define('SITE_PATH','/var/www/adminLTE');
+define('SITE_PATH','/var/www/propel-bootemp');
 define('SITE_PATH_ADMIN', SITE_PATH.'/admin');
 define('SITE_PATH_CONFIG',SITE_PATH.'/config');
-define('SITE_PATH_CONFIG_PROPEL',SITE_PATH.'/config/adminLTE-conf.php');
+define('SITE_PATH_CONFIG_PROPEL',SITE_PATH.'/config/propel-bootemp-conf.php');
 define('SITE_PATH_MODULES',SITE_PATH_ADMIN.'/modules');
-define('SITE_DEBUG',false);
+define('SITE_DEBUG', false);
 
 
 require_once 'drivers.php';
